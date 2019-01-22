7ec33b3e157aa8ff58ea0e9d5a6b4e8237aa27dd
rohit <admin@ornatejewels.com>
2019-01-22 11:08:50 +0530

    22-1 Commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://wowoverseas.com/op/admin/');
+define('HTTP_CATALOG', 'https://wowoverseas.com/op/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://wowoverseas.com/op/admin/');
+define('HTTPS_CATALOG', 'https://wowoverseas.com/op/');
+
+// DIR
+define('DIR_APPLICATION', '/home/wowovers/public_html/op/admin/');
+define('DIR_SYSTEM', '/home/wowovers/public_html/op/system/');
+define('DIR_IMAGE', '/home/wowovers/public_html/op/image/');
+define('DIR_LANGUAGE', '/home/wowovers/public_html/op/admin/language/');
+define('DIR_TEMPLATE', '/home/wowovers/public_html/op/admin/view/template/');
+define('DIR_CONFIG', '/home/wowovers/public_html/op/system/config/');
+define('DIR_CACHE', '/home/wowovers/public_html/op/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/wowovers/public_html/op/system/storage/download/');
+define('DIR_LOGS', '/home/wowovers/public_html/op/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/wowovers/public_html/op/system/storage/modification/');
+define('DIR_UPLOAD', '/home/wowovers/public_html/op/system/storage/upload/');
+define('DIR_CATALOG', '/home/wowovers/public_html/op/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'wowovers_ocart29');
+define('DB_PASSWORD', 'p3S4[14Hl[');
+define('DB_DATABASE', 'wowovers_ocart29');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'octt_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://wowoverseas.com/op/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://wowoverseas.com/op/');
+
+// DIR
+define('DIR_APPLICATION', '/home/wowovers/public_html/op/catalog/');
+define('DIR_SYSTEM', '/home/wowovers/public_html/op/system/');
+define('DIR_IMAGE', '/home/wowovers/public_html/op/image/');
+define('DIR_LANGUAGE', '/home/wowovers/public_html/op/catalog/language/');
+define('DIR_TEMPLATE', '/home/wowovers/public_html/op/catalog/view/theme/');
+define('DIR_CONFIG', '/home/wowovers/public_html/op/system/config/');
+define('DIR_CACHE', '/home/wowovers/public_html/op/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/wowovers/public_html/op/system/storage/download/');
+define('DIR_LOGS', '/home/wowovers/public_html/op/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/wowovers/public_html/op/system/storage/modification/');
+define('DIR_UPLOAD', '/home/wowovers/public_html/op/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'wowovers_ocart29');
+define('DB_PASSWORD', 'p3S4[14Hl[');
+define('DB_DATABASE', 'wowovers_ocart29');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'octt_');
